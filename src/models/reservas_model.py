04e28a3ab3d7632684682d8f4d34475374fda1eb
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2023-11-27 01:15:04 -0300

    fix: correção do tipo de campo do nome

--- src/models/reservas_model.py
+++ src/models/reservas_model.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from typing import Optional
 
 from sqlmodel import Field, Relationship, SQLModel
@@ -9,8 +8,8 @@ from src.models.voos_model import Voo
 class Reserva(SQLModel, table=True):
     __tablename__ = "reservas"
     id: int = Field(default=None, primary_key=True)
-    nome: datetime
+    nome: str
     documento: str
-    codigo_reserva: str
+    codigo_reserva: Optional[str | None] = Field(nullable=False)
     voo_id: int = Field(nullable=False, foreign_key="voos.id")
     voos: Optional[Voo] = Relationship(back_populates="reservas")
